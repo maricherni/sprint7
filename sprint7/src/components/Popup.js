c73afc47a658f2c6ed38f0377eba3b3a6bf2489d
Maria Rodriguez <mary_cherny@msn.com>
2022-07-22 17:45:47 +0200

    Ex6

--- sprint7/src/components/Popup.js
+++ sprint7/src/components/Popup.js
@@ -1,16 +1,25 @@
 import Popup from 'reactjs-popup';
 import { PopupButton, PopupActive } from './Styled';
 
+
+
 export const PagesPopup = (props) =>{
     return(
     <div>
      <Popup 
         trigger={<PopupButton> i </PopupButton>} 
-        position="absolute">
-            <PopupActive><div>
+        position="bottom center"
+        offsetY={100}
+        overlayStyle={{background:'gray', opacity:'50%'}}
+        repositionOnResize={true}
+        arrow={false}
+        >
+        <PopupActive>
+            <div>
                 Puedes elegir el número de páginas que quieres para tu web.<br/>
                 El sitio web estará compuesto de {props.qty} páginas.
-            </div></PopupActive>
+            </div>
+        </PopupActive>
      </Popup> 
     </div>
     )
@@ -19,8 +28,14 @@ export const PagesPopup = (props) =>{
 export const LanguagesPopup = (props) =>{
     return(
     <div>
-      <Popup trigger={<PopupButton> i </PopupButton>} 
-       position="bottom">
+      <Popup 
+        trigger={<PopupButton> i </PopupButton>} 
+        position="bottom center"
+        offsetY={55}
+        overlayStyle={{background:'gray', opacity:'50%'}}
+        repositionOnResize={true}
+        arrow={false}
+        >
         <PopupActive>
                 <div>
                     El sitio web estará en {props.qty} idiomas
--- sprint7/src/components/Styled.js
+++ sprint7/src/components/Styled.js
@@ -85,3 +85,5 @@ export const PopupActive = styled.div `
     text-align: center;
     align-items: flex-end;
 `;
+
+
